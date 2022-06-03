4584a47f270f7ff03a689790e62375707cfd57bd
Jean Vasconcelos <jeanjunior09@gmail.com>
2022-06-03 12:25:05 -0300

    morgan and nodeman

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/products');
 const rotaPedidos = require('./routes/pedidos');
+const res = require('express/lib/response');
+
+// Utilizando morgan para tratar erros 
+app.use(morgan('dev'));
 
 app.use('/products', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
 
 module.exports = app;
